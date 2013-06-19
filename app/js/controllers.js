8139e731ac280ff05f79cca0f82d3055cc4e005e
Frederik Bracke <frederik.bracke@c4j.be>
2013-06-19 15:45:43 +0200

    fixed the datepicker

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,13 +2,15 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
+var app = angular.module('myApp.controllers', ['$strap.directives']).
   controller('MyCtrl1', [function() {
 
   }])
   .controller('MyCtrl2', [function() {
 
-  }])
-    .controller('BasicInfoCtrl', [function(){
+  }]);
 
-    }]);
+   app.controller('BasicInfoCtrl', function($scope, $window, $location){
+        $scope.datepicker = {date: new Date("2012-09-01T00:00:00.000Z")};
+
+    });
