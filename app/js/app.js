8912171518d7bb3a3de25dcdbfdb62c378248a8d
Kristof Verslegers <kristof.verslegers@c4j.be>
2013-06-19 17:21:05 +0200

    Added the login page

--- app/js/app.js
+++ app/js/app.js
@@ -4,9 +4,8 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+    $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'MyCtrl2'});
     $routeProvider.when("/update", { templateUrl:'partials/update.html' });
     $routeProvider.when("/basic-info", { templateUrl:'partials/basic-info.html', controller: 'BasicInfoCtrl' });
-    $routeProvider.otherwise({redirectTo: '/view1'});
+    $routeProvider.otherwise({redirectTo: '/login'});
   }]);
